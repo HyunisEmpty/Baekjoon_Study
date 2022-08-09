86ae317dbd9da9087c4d0e51ab99052f44b3be08
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2022-08-09 21:34:26 +0900

    [Bronze II] Title: 저항, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+value = {"black" : 0, "brown" : 1, "red" : 2, "orange" : 3, "yellow" :4, "green" : 5, "blue" : 6, "violet" : 7, "grey" : 8, "white" : 9}
+gob = {"black" : 1, "brown" : 10, "red" : 100, "orange" : 1000, "yellow" :10000, "green" : 100000, "blue" : 1000000, "violet" : 10000000, "grey" : 100000000, "white" : 1000000000}
+
+a = input()
+b = input()
+c = input()
+print(int(str(value[a]) + str(value[b])) * gob[c])
