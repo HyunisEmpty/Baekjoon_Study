b269216db3968179ddfff0aa779c6f84ae34e63a
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-03-22 23:21:59 +0900

    [Bronze V] Title: Hello World, Time: 56 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace backjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+				  Console.WriteLine("Hello World!");
+        }
+    }
+}
