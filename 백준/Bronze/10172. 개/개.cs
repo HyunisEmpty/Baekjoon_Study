2be0e0a20e97015775c1ea13695d7b38118636ea
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-04-08 13:09:21 +0900

    [Bronze V] Title: 개, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class main {
+	public static void Main() {
+		
+		Console.WriteLine("|\\_/|");
+		Console.WriteLine("|q p|   /}");
+		Console.WriteLine("( 0 )\"\"\"\\");
+		Console.WriteLine("|\"^\"`    |");
+		Console.WriteLine("||_/=\\\\__|");
+		
+		
+	}
+}
