ec64183de229a651b74dd52cd9831fb61ad8e4d2
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-04-23 13:19:30 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 60 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class main {
+	public static void Main() {
+		
+		string input = Console.ReadLine();
+		Console.WriteLine(input.Length);
+		
+	}
+}
