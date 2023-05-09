cd44a224aad0d5552777769fdb3e0071591ff741
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-05-09 20:01:10 +0900

    [Bronze V 11654] Title: 아스키 코드, Time: 64 ms, Memory: 5472 KB -NKLCBHub

---
+++ 백준/Bronze/#11654 아스키 코드/아스키 코드.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class main {
+	public static void Main() {
+		
+		char input = Convert.ToChar(Console.ReadLine());
+		byte Ascii = Convert.ToByte(input);
+		
+		Console.WriteLine(Ascii);
+	} 
+}
