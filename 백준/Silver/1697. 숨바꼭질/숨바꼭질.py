aab9cbe275f063836b0c85da006a2779c6bba4d7
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-05-31 22:57:54 +0900

    [Silver I] Title: 숨바꼭질, Time: 184 ms, Memory: 42772 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -2,7 +2,8 @@ import sys
 from collections import deque
 
 start_position, end_position = map(int, sys.stdin.readline().split())
-second = min_second = 0
+second = 0
+min_second = -1
 
 bfs_set = set()
 bfs_deque = deque([])
@@ -15,24 +16,22 @@ while len(bfs_deque) != 0:
     position, second = bfs_deque.popleft()
 
     if position == end_position:
-        if min_second == 0:
+        if min_second == -1:
             min_second = second
         else:
             if second < min_second:
                 min_second = second
 
+    # 현재 위치가 목적지라면 추가적인 연산을 하지 않는다.
     if position != end_position:
         if position + 1 not in bfs_set and 0 <= position + 1 <= 100000:
             bfs_deque.append((position + 1, second + 1))
-            if position + 1 != end_position:
-                bfs_set.add(position + 1)
+            bfs_set.add(position + 1)
         if position - 1 not in bfs_set and 0 <= position - 1 <= 100000:
             bfs_deque.append((position - 1, second + 1))
-            if position - 1 != end_position:
-                bfs_set.add(position - 1)
+            bfs_set.add(position - 1)
         if position * 2 not in bfs_set and 0 <= position * 2 <= 100000:
             bfs_deque.append((position * 2, second + 1))
-            if position * 2 != end_position:
-                bfs_set.add(position * 2)
+            bfs_set.add(position * 2)
 
 print(min_second)
