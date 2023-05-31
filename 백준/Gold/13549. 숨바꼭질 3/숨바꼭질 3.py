61131247c78ce58d4093b6f533f4c71fc13c0711
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-05-31 22:43:27 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 300 ms, Memory: 44836 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -5,11 +5,23 @@ start_position, end_position = map(int, sys.stdin.readline().split())
 position_list = [-1 for _ in range(0, 1000001)]
 second = 0
 min_second = -1
-
 bfs_deque = deque([])
-
 position_list[start_position] = second
 bfs_deque.append((start_position, second))
+
+
+def NewPosition(new_position, new_second):
+    if 0 <= new_position <= 100000:
+        # 처음 접근 했다면
+        if position_list[new_position] == -1:
+            position_list[new_position] = new_second
+            bfs_deque.append((new_position, new_second))
+        # 접근한 적 있다면
+        else:
+            if new_second < position_list[new_position]:
+                position_list[new_position] = new_second
+                bfs_deque.append((new_position, new_second))
+    
 
 while len(bfs_deque) != 0:
 
@@ -23,35 +35,8 @@ while len(bfs_deque) != 0:
                 min_second = second
 
     if position != end_position:
-        if 0 <= position + 1 <= 100000:
-            # 처음 접근 했다면
-            if position_list[position + 1] == -1:
-                position_list[position + 1] = second + 1
-                bfs_deque.append((position + 1, second + 1))
-            # 접근한 적 있다면
-            else:
-                if second + 1 < position_list[position + 1]:
-                    position_list[position + 1] = second + 1
-                    bfs_deque.append((position + 1, second + 1))
-        if 0 <= position - 1 <= 100000:
-            # 처음 접근 했다면
-            if position_list[position - 1] == -1:
-                position_list[position - 1] = second + 1
-                bfs_deque.append((position - 1, second + 1))
-            # 접근한 적 있다면
-            else:
-                if second + 1 < position_list[position - 1]:
-                    position_list[position - 1] = second + 1
-                    bfs_deque.append((position - 1, second + 1))
-        if 0 <= position * 2 <= 100000:
-            # 처음 접근 했다면
-            if position_list[position * 2] == -1:
-                position_list[position * 2] = second
-                bfs_deque.append((position * 2, second))
-            # 접근한 적 있다면
-            else:
-                if second < position_list[position * 2]:
-                    position_list[position * 2] = second
-                    bfs_deque.append((position * 2, second))
+        NewPosition(position + 1, second + 1)
+        NewPosition(position - 1, second + 1)
+        NewPosition(position * 2, second)
 
 print(min_second)
