cd767870610b148aa11354df109e7d4ec4da2cc4
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-06-02 23:01:44 +0900

    [Silver IV] Title: 큐 2, Time: 2420 ms, Memory: 175188 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().strip())
+queue = deque([])
+
+for i in range(n):
+    command = list(map(str, sys.stdin.readline().split()))
+
+    if command[0] == "push":
+        queue.append(command[1])
+    elif command[0] == "pop":
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif command[0] == "size":
+        print(len(queue))
+    elif command[0] == "empty":
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "front":
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == "back":
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[len(queue)-1])
