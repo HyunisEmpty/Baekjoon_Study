eab99e86fdb2a11648712ceba800b95eda8d7ebe
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-06-02 22:21:39 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 4092 ms, Memory: 45808 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+
+start_position, end_position = map(int, sys.stdin.readline().split())
+second = 0
+min_second = -1
+
+route = str(start_position)
+min_route = ""
+
+bfs_set = set()
+bfs_deque = deque([])
+
+bfs_set.add(start_position)
+bfs_deque.append((start_position, second, route))
+
+while len(bfs_deque) != 0:
+
+    position, second, route = bfs_deque.popleft()
+
+    if position == end_position:
+
+        if min_second == -1:
+            min_second = second
+            min_route = route
+        else:
+            if second < min_second:
+                min_second = second
+                min_route = route
+
+    # 현재 위치가 목적지라면 추가적인 연산을 하지 않는다.
+    if position != end_position:
+        if position + 1 not in bfs_set and 0 <= position + 1 <= 100000:
+            bfs_deque.append((position + 1, second + 1, route + " " + str(position + 1)))
+            bfs_set.add(position + 1)
+        if position - 1 not in bfs_set and 0 <= position - 1 <= 100000:
+            bfs_deque.append((position - 1, second + 1, route + " " + str(position - 1)))
+            bfs_set.add(position - 1)
+        if position * 2 not in bfs_set and 0 <= position * 2 <= 100000:
+            bfs_deque.append((position * 2, second + 1, route + " " + str(position * 2)))
+            bfs_set.add(position * 2)
+
+print(min_second)
+print(min_route)
