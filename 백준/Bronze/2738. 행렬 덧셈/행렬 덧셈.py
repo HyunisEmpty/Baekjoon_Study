49461d0cb4153740e79a895ff8909b909342c82c
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2023-06-05 23:01:48 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,23 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+matrix_a = []
+matrix_b = []
+matrix_c = []
+
+for i in range(n):
+    matrix_a.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(n):
+    matrix_b.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(n):
+    for j in range(m):
+        matrix_a[i][j] += matrix_b[i][j]
+
+for i in range(n):
+    print(" ".join(map(str, matrix_a[i])))
+
+
+
