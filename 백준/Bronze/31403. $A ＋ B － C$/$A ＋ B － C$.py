616231c9bd636e4bd007ceead1a71c200fc4ebd6
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2024-07-17 23:38:33 +0900

    [Bronze IV] Title: $A + B - C$, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,8 @@
+import sys
+
+a = int(sys.stdin.readline().strip())
+b = int(sys.stdin.readline().strip())
+c = int(sys.stdin.readline().strip())
+
+print(a + b - c)
+print(int(str(a) + str(b)) - c)
