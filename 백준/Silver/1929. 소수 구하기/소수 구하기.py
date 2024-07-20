355204146afc223c14cfc0417432bab9ce111bf5
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2024-07-20 15:19:29 +0900

    [Silver III] Title: 소수 구하기, Time: 180 ms, Memory: 118240 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -9,13 +9,13 @@ n_bool[1] = False   # 0은 소수 판별에서 제외
 index = 2
 
 # n_bool의 모든 원소에 한번씩 접근
-while index < n:
+while index ** 2 <= n:
 
     cnt = 2
 
     # 현재 값의 배수를 제거
     if n_bool[index]:
-        
+
         while index * cnt <= n:
             n_bool[index * cnt] = False
             cnt += 1
