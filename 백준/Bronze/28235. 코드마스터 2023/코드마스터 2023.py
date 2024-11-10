da6140aa1de861c3cd557caba2464a2b724e8e3a
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2024-11-10 22:33:36 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+import sys
+
+word = sys.stdin.readline().strip()
+
+if word == "SONGDO":
+    print("HIGHSCHOOL")
+elif word == "CODE":
+    print("MASTER")
+elif word == "2023":
+    print("0611")
+else:
+    print("CONTEST")
