d95d316933e1fac535addb1bbac2706b61a1f30d
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2024-11-15 14:44:47 +0900

    [Gold V] Title: 집합의 표현, Time: 244 ms, Memory: 120204 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -3,36 +3,33 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 n_list = [i for i in range(n + 1)]
 
+
+def FindRootNode(node1):
+
+    global n_list
+
+    while n_list[node1] != node1:  # node1이 루트 노드가 아니라면 
+        node1 = n_list[node1]     # 현재 노드를 부모 노드로 변경
+        
+    return node1
+
+
 for _ in range(m):
     command, a, b = map(int, sys.stdin.readline().split())
 
+    a_root = FindRootNode(a)
+    b_root = FindRootNode(b)
+
     if command == 0:     # 합집합 연산
 
-        a_root = a
-        b_root = b
-
-        while n_list[a_root] != a_root:   # a에는 root 노드가 저장됨
-            a_root = n_list[a_root]
-
-        while n_list[b_root] != b_root:   # b에는 root 노드가 저장됨
-            b_root = n_list[b_root]
+        n_list[b_root] = a_root             # b집합을 a집합과 합치고 b의 루트 노드를 a루트의 자식 노드로 변경
 
         # 경로 압축
-        n_list[b_root] = a_root
         n_list[b] = a_root
         n_list[a] = a_root
 
     elif command == 1:
-
-        a_root = a
-        b_root = b
-
-        while n_list[a_root] != a_root:  # a에는 root 노드가 저장됨
-            a_root = n_list[a_root]
-
-        while n_list[b_root] != b_root:  # b에는 root 노드가 저장됨
-            b_root = n_list[b_root]
-
+        
         if a_root == b_root:
             print("YES")
         else:
