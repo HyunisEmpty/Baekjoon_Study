59d7e42419a8564c8cec8c95254d0d1e7a7e18c1
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2024-11-26 13:52:00 +0900

    [Gold IV] Title: 최단경로, Time: 628 ms, Memory: 164812 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,42 +1,39 @@
 import sys
 import heapq
-from collections import deque
 
-v, e = map(int, sys.stdin.readline().split())       # v: 노드 개수, e: 간선 개수
-start_v = int(sys.stdin.readline().strip())         # 시작 노드
-v_list = [[] for _ in range(v + 1)]                 # 노드 사이의 연결 정보 저장
+n, m = map(int, sys.stdin.readline().split())
+x = int(sys.stdin.readline().strip())
+
 INF = int(1e9)
-shortest_distance_list = [INF for _ in range(v + 1)]         # 시작 노드로 부터 최단 거리 저장
-min_heap = []
-visited = set()     # 방문 노드 저장
+n_list = [[] for _ in range(n + 1)]
+shortest_distance_list = [INF for _ in range(n + 1)]
+min_queue = []
+visited = set()
 
+for _ in range(m):  # 간선 정보 입력
+    start, end, weight = map(int, sys.stdin.readline().split())
+    n_list[start].append((end, weight))
 
-# 간선 정보 입력
-for _ in range(e):
+heapq.heappush(min_queue, (0, x))
+shortest_distance_list[x] = 0
 
-    # node1에서 node2로 가는 간선 가중치는 weight
-    node1, node2, weight = map(int, sys.stdin.readline().split())
+while min_queue:
 
-    v_list[node1].append((weight, node2))
+    weight, node = heapq.heappop(min_queue)
 
-heapq.heappush(min_heap, (0, start_v))
-
-while min_heap:
-
-    weight, node = heapq.heappop(min_heap)
-
-    if node not in visited:     # 방문 한적 없는 경우
+    if node not in visited:  # 방문 여부 판단
         visited.add(node)
-
-        # 주변 노드 접근
-        for info in v_list[node]:
-            neighbor_weight, neighbor = info
-            heapq.heappush(min_heap, (weight + neighbor_weight, neighbor))
-
         shortest_distance_list[node] = weight
+
+        for end, end_weight in n_list[node]:    # 인접 노드 접근
+
+            if end not in visited:
+                heapq.heappush(min_queue, (end_weight + weight, end))
 
 for i in range(1, len(shortest_distance_list)):
     if shortest_distance_list[i] == INF:
         print("INF")
     else:
         print(shortest_distance_list[i])
+
+
