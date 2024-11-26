f857f189d42acb2fbf9fd40da8854e6f04fb6285
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2024-11-26 14:07:37 +0900

    [Gold III] Title: 파티, Time: 184 ms, Memory: 112696 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -6,7 +6,8 @@ n, m, x = map(int, sys.stdin.readline().split())
 INF = int(1e9)
 back_list = [[] for _ in range(n + 1)]
 go_list = [[] for _ in range(n + 1)]
-shortest_distance_list = [INF for _ in range(n + 1)]
+back_shortest_distance_list = [INF for _ in range(n + 1)]
+go_shortest_distance_list = [INF for _ in range(n + 1)]
 min_queue = []
 visited = set()
 
@@ -16,7 +17,7 @@ for _ in range(m):  # 간선 정보 입력
     go_list[end].append((start, weight))
 
 heapq.heappush(min_queue, (0, x))
-shortest_distance_list[x] = 0
+back_shortest_distance_list[x] = 0
 
 # 시작 섬에서 각자 섬으로 갈때 다익스트라 알고리즘
 while min_queue:
@@ -25,18 +26,17 @@ while min_queue:
 
     if node not in visited:  # 방문 여부 판단
         visited.add(node)
-        shortest_distance_list[node] = weight
+        back_shortest_distance_list[node] = weight
 
         for end, end_weight in back_list[node]:    # 인접 노드 접근
 
             if end not in visited:
                 heapq.heappush(min_queue, (end_weight + weight, end))
 
-shortest_distance_list2 = [INF for _ in range(n + 1)]
-min_queue = []
+min_queue.clear()       # 최소힙 초기화 
+visited.clear()         # 집합 초기화 
 heapq.heappush(min_queue, (0, x))
-shortest_distance_list2[x] = 0
-visited = set()
+go_shortest_distance_list[x] = 0
 
 # 각자 섬에서 시작 섬으로 올 때 다익스트라 알고리즘
 while min_queue:
@@ -45,7 +45,7 @@ while min_queue:
 
     if node not in visited:  # 방문 여부 판단
         visited.add(node)
-        shortest_distance_list2[node] = weight
+        go_shortest_distance_list[node] = weight
 
         for end, end_weight in go_list[node]:    # 인접 노드 접근
 
@@ -55,7 +55,7 @@ while min_queue:
 answer = 0
 for i in range(1, n + 1):
 
-    answer = max(answer, shortest_distance_list2[i] + shortest_distance_list[i])
+    answer = max(answer, go_shortest_distance_list[i] + back_shortest_distance_list[i])
 
 print(answer)
 
