e5a9a8c0eec3091bb9fd9da4f787ab1e3a99cd96
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2024-12-05 16:27:11 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 5124 ms, Memory: 259556 KB -BaekjoonHub

---
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
@@ -0,0 +1,38 @@
+import sys
+import heapq
+
+n, m, k = map(int, sys.stdin.readline().split())    # n : 노드, m : 간선, k : k번째 최단경로
+
+n_list = [[] for _ in range(n + 1)]
+for _ in range(m):      # 간선 정보 입력
+    a, b, c = map(int, sys.stdin.readline().split())
+    n_list[a].append((b, c))
+
+INF = int(1e9)
+shortest_distance_list = [INF for _ in range(n + 1)]    # 최단 거리 테이블
+shortest_distance_update_list = [0 for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]                 # 방문 여부 저장
+min_heap = []
+
+shortest_distance_list[1] = 0   # 시작 노드
+heapq.heappush(min_heap, (shortest_distance_list[1], 1))
+
+while min_heap:
+
+    weight, node = heapq.heappop(min_heap)
+
+    if shortest_distance_update_list[node] != k:   # 방문 하지 않은 경우
+        shortest_distance_list[node] = weight
+        shortest_distance_update_list[node] += 1
+
+        for neighbor, neighbor_weight in n_list[node]:
+
+            heapq.heappush(min_heap, (weight + neighbor_weight, neighbor))
+
+# print(shortest_distance_update_list)
+for node in range(1, n + 1):
+
+    if shortest_distance_update_list[node] == k:    # k번째 경로가 존재 하는 경우
+        print(shortest_distance_list[node])
+    else:
+        print(-1)
